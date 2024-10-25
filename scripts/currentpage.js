b75699a91090c326eadab2f80eadf1cb49981e92
jcalhou9 <jcalhou9@email.cpcc.edu>
2024-10-25 12:03:59 -0400

    Update currentpage.js

--- scripts/currentpage.js
+++ scripts/currentpage.js
@@ -2,9 +2,8 @@ const active = window.location.pathname;
 const navLinks = document.querySelectorAll('.nav-bar li a');
 navLinks.forEach((link) => {
     if (link.href.includes('index.html')) {
-        link.parentElement.classList.add('currentpage');
-    }
-    else if (link.href.includes(`${active}`)) {
-        link.parentElement.classList.add('currentpage');
+            link.parentElement.classList.add('currentpage');
+    } else if (link.href.includes(`${active}`)) {
+            link.parentElement.classList.add('currentpage');
     }
 });
