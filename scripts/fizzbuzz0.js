21dfaaf68a0574495a30bd99aedcaaa8d76c609b
jcalhou9 <jcalhou9@email.cpcc.edu>
2024-10-31 20:55:46 -0400

    FB0 feedback update
    -added form for required input instead of prompt
    -changed onclick to onsubmit and added prevent default
    -inverted button colors
    -added reset button
    -added link to rwd in footer
    -added count validation for FB0
    -added h3 for FB

--- scripts/fizzbuzz0.js
+++ scripts/fizzbuzz0.js
@@ -1,24 +1,38 @@
 /* Jeremy Calhoun
 WEB115-N801 Fall 24 */
 
-document.getElementById("submit").onclick = function greetUser() {
+document.getElementById("reset").onclick = function reset() {
+    document.getElementById("greeting").textContent = "Let's go again!";
+    document.getElementById("output").textContent = '';
+};
+
+document.getElementById("form").onsubmit = function greetUser(submit) {
+    submit.preventDefault();
 
     const firstName = document.getElementById("first-name").value;
     const middleInitial = document.getElementById("middle-initial").value;
     const lastName = document.getElementById("last-name").value;
 
     if (!firstName || !lastName) {
-        alert("First and last names are required!");
         return;
     }
 
+    let count;
+    while (true) {
+        count = prompt(`How high do you want to count, ${firstName}?`);
+        if (count === null) return;
+        count = Number(count);
+        if (!isNaN(count) && count > 0) break;
+        alert("Please enter a valid positive number.");
+    }
+    
     const fullName = middleInitial ? `${firstName} ${middleInitial}. ${lastName}` : `${firstName} ${lastName}`;
     
     const greetingElement = document.getElementById("greeting");
     greetingElement.textContent = `Welcome to Fizz Buzz, ${fullName}!`;
 
-    const count = prompt(`How high do you want to count, ${firstName}?`);
-    
+
+
     const outputList = document.getElementById("output");
     outputList.textContent = "";
 
--- scripts/fizzbuzz2.js
+++ scripts/fizzbuzz2.js
@@ -1,14 +1,19 @@
 /* Jeremy Calhoun
 WEB115-N801 Fall 24 */
 
-document.getElementById("submit").onclick = function greetUser() {
+document.getElementById("reset").onclick = function reset() {
+    document.getElementById("greeting").textContent = "Let's go again!";
+    document.getElementById("output").textContent = '';
+};
+
+document.getElementById("form").onsubmit = function greetUser(submit) {
+    submit.preventDefault();
 
     const firstName = document.getElementById("first-name").value;
     const middleInitial = document.getElementById("middle-initial").value;
     const lastName = document.getElementById("last-name").value;
 
     if (!firstName || !lastName) {
-        alert("First and last names are required!");
         return;
     }
 
